218d12ca4123901895f639d7c8c9472c34101d04
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2023-09-14 08:53:54 +0530

    Delete extra.js

--- extra.js
+++
@@ -1,6 +0,0 @@
-let contestToProblemsMap = new Map();
-const prob = {};
-let arr = new Array();
-if(contestToProblemsMap.has(prob.contestId)) arr = contestToProblemsMap.get(prob.contestId);
-arr.push({"contestId": prob.contestId,"index":prob.index,"rating": prob.rating});
-contestToProblemsMap.set(prob.contestId,arr);
